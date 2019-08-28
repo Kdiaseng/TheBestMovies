f62c7c509f7f0ae4e462552f3cada4b8a3d198a7
kaleb.silva <kaleb.silva@itbam.org.br>
2019-08-28 18:57:28 -0400

    criado activity de cadastro de filmes

---
+++ app/src/main/java/com/kdias/thebestmovies/features/cadastroFilmes/CadastroFilmesActivity.kt
@@ -0,0 +1,13 @@
+package com.kdias.thebestmovies.features.cadastroFilmes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kdias.thebestmovies.R
+
+class CadastroFilmesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_filmes)
+    }
+}
