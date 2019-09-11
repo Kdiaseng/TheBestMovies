56b7cf4b97d3f752e98ed8871c91b4251879c0f0
kaleb.dias <kalebdias.oficial@gmail.com>
2019-09-11 19:29:36 -0400

    layout de tela de detalhes de filmes

---
+++ app/src/main/java/com/kdias/thebestmovies/features/detalhesMovies/DetalhesMoviesActivity.kt
@@ -0,0 +1,13 @@
+package com.kdias.thebestmovies.features.detalhesMovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kdias.thebestmovies.R
+
+class DetalhesMoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_movies)
+    }
+}
