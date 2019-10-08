74a2570ad1c6b216751afe68265becb36ad6ed3b
kaleb.silva <kaleb.silva@itbam.org.br>
2019-10-08 16:59:56 -0400

    removidas dependecias com erros

--- app/src/main/java/com/kdias/thebestmovies/features/utils/Bindings.kt
+++ app/src/main/java/com/kdias/thebestmovies/features/utils/Bindings.kt
@@ -3,7 +3,7 @@ package com.kdias.thebestmovies.features.utils
 import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.anderson.androidtrend.utlis.GlideApp
+//import com.anderson.androidtrend.utlis.GlideApp
 import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("imageUrl")
@@ -13,10 +13,10 @@ fun loadImage(view: ImageView, poster: String?) {
 
     poster?.let{
 
-        GlideApp.with(view.context)
-            .load(poster)
-            .apply(RequestOptions.circleCropTransform())
-            .into(view)
+//        GlideApp.with(view.context)
+//            .load(poster)
+//            .apply(RequestOptions.circleCropTransform())
+//            .into(view)
     }
 
 }
